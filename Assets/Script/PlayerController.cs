a0889c6307270fc078c729e588fd90044e156d52
丸山 真太郎 <smaruyama@crooz.co.jp>
2016-04-18 11:17:15 +0900

    First commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	// speedを制御する
+	public float speed = 10;
+
+	void FixedUpdate ()
+	{
+		//  入力をxとzに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		// 同一のGameObjectが持つRigidbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		rigidbody.AddForce (x * 10, 0, z * 10);
+	}
+}
